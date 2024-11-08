5b1888c387fe7ed192194ac172ffe20030103fda
uzigoral <163141960+uzigoral@users.noreply.github.com>
2024-11-08 09:33:51 -0300

    Se mejoro la descripcion

---
+++ Nosotros.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Nosotros = () => {
+  return (
+    <div>
+      <h1>Nosotros</h1>
+      <p>
+        Somos una empresa dedicada a las hamburguesas, con locales distrisbuidos en toda la plata y un sitio web online. 
+      </p>
+      <p>
+        Fundada en 2024, nuestra misión es lograr una buena reputacion entre los fanaticos de las hamburguesas.
+      </p>
+      <br>
+      </br>
+      <p>Dueños de la empresa:</p>
+      <ul>
+        <li>Marcelino Leandro</li>
+        <li>Tessari Ignacio</li>
+        <li>Goral Uziel</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nosotros;
